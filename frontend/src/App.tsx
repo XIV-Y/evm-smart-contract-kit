a863a5ede5aa894a89c03074de11f9a2d7044166
XIV-Y <yutodazepa.g@gmail.com>
2025-05-21 21:44:57 +0900

    #4 feat: ethers5 でのウォレット接続機能実装

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,47 @@
-function App() {
-  return <></>;
-}
+import {
+  createAppKit,
+  useAppKitAccount,
+  useDisconnect,
+} from "@reown/appkit/react";
+import { Ethers5Adapter } from "@reown/appkit-adapter-ethers5";
+import { sepolia } from "@reown/appkit/networks";
 
-export default App;
+const projectId = import.meta.env.VITE_PROJECT_ID;
+
+const metadata = {
+  name: "eth-erc20-token-react",
+  description: "AppKit Example",
+  url: "",
+  icons: [],
+};
+
+createAppKit({
+  adapters: [new Ethers5Adapter()],
+  networks: [sepolia],
+  metadata,
+  projectId,
+  features: {
+    analytics: true,
+  },
+});
+
+export default function App() {
+  const { address } = useAppKitAccount();
+  const { disconnect } = useDisconnect();
+
+  const handleDisconnect = async () => {
+    await disconnect();
+  };
+
+  console.log("address", address);
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen w-full bg-[#1a202c]">
+      <w3m-button />
+
+      <button className="text-white bg" onClick={handleDisconnect}>
+        Disconnect
+      </button>
+    </div>
+  );
+}
